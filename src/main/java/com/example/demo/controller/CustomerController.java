df06d12d2f9fb6a861e462d535a4c2bd9f491737
Dhivyasellathurai <dhivyasellathurai@gmail.com>
2024-07-29 11:10:20 +0530

    cors error

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.example.demo.service.CustomerService;
 import jakarta.servlet.http.HttpSession;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
